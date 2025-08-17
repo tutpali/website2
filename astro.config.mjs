c28810512ea8fc348bb8e91026553919b798fdae
tutpali <HECTOR.S.CAMACHO@GMAIL.COM>
2025-08-17 16:55:22 +0200

    Fix Netlify configuration to remove Deno dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
+  output: 'static',
+  adapter: netlify({
+    edgeMiddleware: false
+  }),
   integrations: [tailwind()],
-  });
+});
